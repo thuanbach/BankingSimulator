30ccb3821bc0569c54a59ab9f83a9c3af19a63e5
Thuan Bach <Thuan Bach@PF1P1NM9.nclan.netcompany.dk>
2021-10-20 15:35:13 +0700

    Initiate C++ Banking Project Project

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Oct 20, 2021
+ *      Author: Thuan Bach
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	cout << "HELLO WORLD";
+
+	return 0;
+}
